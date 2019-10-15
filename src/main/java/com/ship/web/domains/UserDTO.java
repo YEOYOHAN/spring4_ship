2d36f30e4000c73c8565906018d1c346a92e2116
maybehan <maybehans@gmail.com>
2019-10-15 18:16:30 +0900

    2019-10-15 18:16

--- src/main/java/com/ship/web/domains/UserDTO.java
+++ src/main/java/com/ship/web/domains/UserDTO.java
@@ -7,5 +7,6 @@ import lombok.Data;
 public class UserDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-	private String uid, uname, upw, age;
+	private String uId, uName, uPw, age, gender, loc, tel, email,
+    uPoint, score, mvp, win, hitmap, km, heart, author, lolName;
 }
