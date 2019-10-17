9c5a4e46a32e9aa1e8fe0c33948bf336f8859db2
maybehan <maybehans@gmail.com>
2019-10-17 19:10:16 +0900

    2019-10-17 19:10

--- src/main/java/com/ship/web/controllers/UserController.java
+++ src/main/java/com/ship/web/controllers/UserController.java
@@ -1,34 +1,34 @@
 package com.ship.web.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ship.web.domains.UserDTO;
 import com.ship.web.serviceimpls.UserServiceImpl;
-import com.ship.web.services.UserService;
 
 @Controller
 @RequestMapping("/user/*")
 public class UserController {
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
-	@Autowired UserServiceImpl userService;
 	
-	@GetMapping("/count")
-	public String count(Model model) {
-		int count = userService.countUser();
-		model.addAttribute("count", count);
-		return "home";
-	}
-	
-	@GetMapping("/info")
-	public String findUserByUid(@RequestParam("uid") String uid,
-			@RequestParam("upw") String upw) {
-		//
-		return "home";
+	@PostMapping("/join")
+	public @ResponseBody Map<?,?> join(@RequestBody UserDTO user){
+		logger.info("ajax가 보낸 아이디 와 비번 {} ", user.getuId()+", "+user.getuPw());
+		HashMap<String, String> map = new HashMap<>();
+		map.put("uId", user.getuId());
+		map.put("uPw", user.getuPw());
+		logger.info("map에 담김 아이디와비번 {}", map.get("uId")+" ,"+map.get("uPw"));
+		return map;
 	}
 }
--- src/main/java/com/ship/web/domains/UserDTO.java
+++ src/main/java/com/ship/web/domains/UserDTO.java
@@ -3,10 +3,30 @@ import java.io.Serializable;
 import org.springframework.stereotype.Component;
 import lombok.Data;
 
-@Data @Component
+@Data
+@Component
 public class UserDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
 	private String uId, uName, uPw, age, gender, loc, tel, email,
     uPoint, score, mvp, win, hitmap, km, heart, author, lolName;
+
+	public String getuId() {
+		return uId;
+	}
+
+	public void setuId(String uId) {
+		this.uId = uId;
+	}
+
+	public String getuPw() {
+		return uPw;
+	}
+
+	public void setuPw(String uPw) {
+		this.uPw = uPw;
+	}
+	
+	
 }
